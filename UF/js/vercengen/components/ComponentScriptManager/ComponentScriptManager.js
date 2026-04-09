a6dd4691ba77dd56b4d8ac1ac5770723fd07ff13
Vis Tacitus <vistacitus@gmail.com>
2026-04-09 05:32:46 +0000

    Update ComponentScriptManager.js

--- UF/js/vercengen/components/ComponentScriptManager/ComponentScriptManager.js
+++ UF/js/vercengen/components/ComponentScriptManager/ComponentScriptManager.js
@@ -193,8 +193,10 @@ ve.ScriptManager = class extends ve.Component {
 					let new_folder_path = path.resolve(this.leftbar_file_explorer.v);
 					
 					this.config.project_folder = new_folder_path;
+          this._settings.project_folder = new_folder_path;
 					ve.ScriptManager._indexDocumentation.call(this, this.bottombar_status_el);
           ve.ScriptManager._saveConfig.call(this);
+          ve.ScriptManager.saveSettings();
 					veToast(loc("ve.registry.localisation.ScriptManager_toast_changed_project_folder", new_folder_path));
 				}, { 
 					name: "<icon>gite</icon>",
@@ -326,7 +328,7 @@ ve.ScriptManager = class extends ve.Component {
 						background: new ve.Text(this._settings.background_image, {
 							name: loc("ve.registry.localisation.ScriptManager_background"),
 							onuserchange: (v) => {
-								if (v.isURL()) v = `url(${v})`;
+								if (String.isURL()) v = `url(${v})`;
 								
 								this._settings.background_image = v;
 								this.loadSettings({ background_image: this._settings.background_image });
