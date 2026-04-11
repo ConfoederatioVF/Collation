ec0f970c2ee9474dcf5db5ccbfed81e9f60747b8
Vis Tacitus <vistacitus@confoederatio.org>
2026-04-11 22:24:34 +0000

    began refactoring ve.CRUD

--- UF/js/vercengen/components/ComponentCRUD.js
+++ UF/js/vercengen/components/ComponentCRUD.js
@@ -3,8 +3,8 @@
  *
  * - `arg0_value`: {@link Array}<{@link any}>
  * - `arg1_options`: {@link Object}
- *   - `.draw_function`: {@link function}(v:{@link any}) | {@link Array}<{@link any}> - How to parse Array elements in the dataframe into rows, excluding the selection row.
- *   - `.header`: {@link Array}<{@link string}>
+ *   - `.header=["Selected", "Index", ...]`: {@link Array}<{@link string}>
+ *   - `.special_function`: {@link function}(v:{@link any}) | {@link Array}<{@link any}> - How to parse Array elements in the dataframe into rows, excluding the selection row.
  *
  *   - `.filters`: {@link Array}<{@link Object}> - [{ name: "All" }] by default.
  *     - `[n].name`: {@link string} - The name of the given tab.
@@ -20,16 +20,20 @@
  * - `.searchbar`: {@link ve.SearchSelect}
  * - `.table`: {@link ve.Table}
  * - `.table_array`: {@link Array}<{@link Array}<{@link any}>>
+ * - `.table_map`: {@link Array}<{@link Object}{ <value_id>: { value:{@link any}, row:{@link any}[] } }>
  */
 ve.CRUD = class extends ve.Component {
   constructor (arg0_value, arg1_options) {
     //Convert from parameters
-    let value = (arg0_value) ? arg0_value : [];
+    let value = (arg0_value) ? Array.toArray(arg0_value) : [];
     let options = (arg1_options) ? arg1_options : {};
       super(options);
     
     //Initialise options
     if (!options.filters) options.filters = [{ name: "All" }];
+    let new_header = ["Selected", "Index"];
+      if (options.header) new_header = new_header.concat(options.header);
+      options.header = new_header;
     
     //Declare local instance variables
     this.element = document.createElement("div");
@@ -37,10 +41,46 @@ ve.CRUD = class extends ve.Component {
       this.element.instance = this;
       HTML.setAttributesObject(this.element, options.attributes);
     this.options = options;
+    this.value = value;
+    
+    //Call this.draw()
+    this.from_binding_fire_silently = true;
+    this.v = value;
+    delete this.from_binding_fire_silently;
+  }
+  
+  get v () {
+    //Return statement
+    return this.value;
+  }
+  
+  set v (arg0_value) {
+    //Convert from parameters
+    let value = Array.toArray(arg0_value);
+    
+    //Set value and call draw
+    this.value = value;
+    this.draw();
   }
   
   draw () {
+    //Declare local instance variables
+    this.element.innerHTML = "";
+    this.searchbar = new ve.SearchSelect({}, {
+      hide_filter: true,
+      onuserchange: (v, e) => {
+        //Declare local instance variables
+        let search_value = e.search_value;
+      },
+      ...this.options.searchbar_options
+    });
+    this.table = new ve.Table(this.getTable(), {
+      disable_hide_columns: [0]
+    });
     
+    //Bind elements in order
+    this.searchbar.bind(this.element);
+    this.table.bind(this.element);
   }
 
   filterTable (arg0_options) {
@@ -49,6 +89,70 @@ ve.CRUD = class extends ve.Component {
   }
   
   getTable () {
+    //Declare local instance variables
+    this.table_array = []; //[[select_button, ...draw_function(value[n])], ...]
+    this.table_map = {}; //{ <value_id>: { value: any, row: any[] } }
     
+    //Set header
+    this.table_array.push(this.options.header);
+    
+    //Populate table_array from value
+    for (let i = 0; i < this.value.length; i++) {
+      let local_array = [];
+      let select_component;
+      
+      //Set local_array
+      //Select column
+      {
+        select_component = veCheckbox(this.value[i]?.selected, {
+          attributes: {
+            "crud-select": "true",
+            "data-value": String(this.value[i]?.selected)
+          },
+          onuserchange: (v, e) => {
+            e.element.setAttribute("data-value", String(v));
+            this.value[i].selected = v;
+          }
+        });
+        select_component.element.value = this.value[i];
+        
+        local_array.push(select_component.element);
+      }
+      
+      //Push index
+      local_array.push(i);
+      
+      //Push everything else from this.options.special_function
+      let row_value = this.options.special_function(this.value[i]);
+      
+      if (row_value)
+        for (let x = 0; x < row_value.length; x++)
+          local_array.push(row_value[x]);
+      
+      //Push local_array to table_array
+      this.table_array.push(local_array);
+      
+      this.table_map[(this.value[i].id) ? this.value[i].id : i] = {
+        value: this.value[i],
+        row: local_array
+      };
+    }
+    
+    //Return statement
+    return this.table_array;
+  }
+  
+  /**
+   * Redraws selection boxes for the present component.
+   * - Method of: {@link ve.CRUD}
+   */
+  redrawSelections () {
+    Object.iterate(this.table_map, (local_key, local_value) => {
+      let is_selected = local_value.value?.selected;
+      let local_checkbox = local_value.row[0].instance;
+      
+      local_checkbox.v = is_selected;
+      local_checkbox.element.setAttribute("data-value", is_selected);
+    });
   }
 };
--- core/ui/windows/UI_FeatureLayerWindow.js
+++ core/ui/windows/UI_FeatureLayerWindow.js
@@ -1,173 +1,105 @@
-global.UI_FeatureLayerWindow = class extends ve.Class { //[WIP] - Finish class body
-	constructor (arg0_layer_obj) {
-		//Convert from parameters
+global.UI_FeatureLayerWindow = class extends ve.Class {
+	constructor(arg0_layer_obj) {
+		// Convert from parameters
 		let layer_obj = arg0_layer_obj;
-			super();
-			
-		//Declare local instance variables
+		super();
+		
+		// Declare local instance variables
 		this.layer = layer_obj;
 	}
-
-  /**
-   * Returns an {@link Object}<{@link ve.Component}> representing the current UI_FeatureLayerWIndow element that can be bound; removes the previous `.interface` if present.
-	 * 
+	
+	/**
+	 * Returns an Object<ve.Component> representing the current UI_FeatureLayerWindow
+	 * element that can be bound; removes the previous .interface if present.
+	 *
 	 * @returns {ve.Interface}
-   */
-  draw () {
-    super.close("instance");
-
-    //Declare local instance variables
-    let geometry_table_array = this.getGeometryTable({ view_tags: true });
-
-    this.interface = veInterface({
-      searchbar: new ve.SearchSelect({}, {
-        hide_filter: true,
-        onuserchange: (v, e) => {
-          //Declare local instance variables
-          let search_value = e.search_value;
-        }
-      }),
-      geometry_table: new ve.Table(geometry_table_array, {
-        disable_hide_columns: [0]
-      })
-    }, { is_folder: false });
-
-    super.open("instance", { 
-      can_rename: false, //[WIP] - Should be able to rename layers from here in future
-      name: (this.layer.name) ? this.layer.name : "Layer"
-    });
-
-    //Return statement
-    return this.interface;
-  }
-	
-	filterGeometryTable (arg0_options) {
+	 */
+	draw () {
+		super.close("instance");
 		
-	}
-	
-  /**
-   * Returns a {@link naissance.Geometry} Table array that can be passed to {@link ve.Table}.
-   * - Method of: {@link UI_FeatureLayerWindow}
-   * 
-   * @param {Object} [arg0_options]
-   *  @param {boolean} [arg0_options.view_tags=false]
-   */
-	getGeometryTable (arg0_options) {
-    //Convert from parameters
-    let options = (arg0_options) ? arg0_options : {};
-
-		//Declare local instance variables
-    let _redrawSelections = () => {
-      Object.iterate(table_map, (local_key, local_value) => {
-        let is_selected = local_value.geometry?.selected;
-        let local_checkbox = local_value.row[0].instance;
-
-        local_checkbox.v = is_selected;
-        local_checkbox.element.setAttribute("data-value", is_selected);
-      });
-    };
-		let table_array = []; //[[select_button, index, geometry_type, geometry_name, actions_bar]];
-    let table_map = {}; //{ <geometry_id>: { geometry: naissance.Geometry, row: any[] } }
+		// Declare local instance variables
+		let all_geometries = this.layer.getAllGeometries();
 		
-		//Populate table_array from entities in this.layer
-		let all_entities = this.layer.getAllGeometries();
-		
-		//Initalise header
-    if (!options.view_tags) {
-		  table_array.push(["Selected", "Index", "Type", "Name", "Actions"]);
-    } else {
-      table_array.push(["Selected", "Index", "Type", "Name", "Tags", "Actions"]);
-    }
-		
-		//Iterate over all_entities and push it to table_array
-		for (let i = 0; i < all_entities.length; i++) {
-			let local_array = [];
-			let local_geometry = all_entities[i];
-			let local_geometry_name = local_geometry.name;
-				if (!local_geometry_name)
-					if (local_geometry.class_name) {
-						local_geometry_name = local_geometry.class_name;
-					} else {
-						local_geometry_name = `Geometry`;
+		// Define the CRUD component
+		this.crud = new ve.CRUD(all_geometries, {
+			header: ["Type", "Name", "Tags", "Actions"],
+			special_function: (local_geometry) => {
+				let local_array = [];
+				
+				// 1. Type column
+				local_array.push(
+					local_geometry.class_name ? local_geometry.class_name : "Geometry"
+				);
+				
+				// 2. Name column
+				let name_component = veText(local_geometry.name, {
+					attributes: {
+						"data-value": local_geometry.name,
+					},
+					onprogramchange: (v, e) => {
+						e.element.setAttribute("data-value", v);
+						e.v = v;
+					},
+					onuserchange: (v, e) => {
+						e.element.setAttribute("data-value", v);
+						local_geometry.name = v;
+					},
+				});
+				local_array.push(name_component.element);
+				
+				// 3. Tags column
+				let local_geometry_tags = local_geometry?.metadata?.tags;
+				local_array.push(
+					Array.isArray(local_geometry_tags) ? local_geometry_tags.join(", ") : ""
+				);
+				
+				// 4. Actions column
+				let actions_bar_el = local_geometry.getActionsBarElement();
+				let brush_button = veButton(
+					(v, e) => {
+						if (main.brush.selected_geometry?.id !== e.element.geometry?.id) {
+							main.brush.selected_geometry = e.element.geometry;
+							local_geometry.selected = true;
+							this.crud.redrawSelections();
+						}
+					},
+					{
+						name: "<icon>brush</icon>",
+						tooltip: "Move to Brush",
 					}
-      let select_component;
-			
-			//Set local_array
-      //Select column
-      {
-        select_component = veCheckbox(local_geometry.selected, { //[WIP] - onprogramchange doesn't fire, no binding
-          attributes: {
-            "naissance-geometry-select": "true",
-            "data-value": String(local_geometry.selected)
-          },
-          onuserchange: (v, e) => {
-            e.element.setAttribute("data-value", String(v));
-            local_geometry.selected = v;
-          }
-        });
-        select_component.element.geometry = local_geometry;
-
-			  local_array.push(select_component.element);
-      }
-			local_array.push(i);
-			local_array.push((local_geometry.class_name) ? local_geometry.class_name : "Geometry");
-      //Name column
-      {
-        let name_component = veText(local_geometry_name, { //[WIP] - onprogramchange doesn't fire, no binding
-          attributes: { 
-            "data-value": local_geometry_name
-          },
-          onprogramchange: (v, e) => {
-            e.element.setAttribute("data-value", v);
-            e.v = v;
-          },
-          onuserchange: (v, e) => {
-            e.element.setAttribute("data-value", v);
-            local_geometry.name = v;
-          }
-        });
-
-        local_array.push(name_component.element);
-      }
-			
-      if (options.view_tags) {
-        let local_geometry_tags = local_geometry?.metadata?.tags;
-
-        if (local_geometry_tags && Array.isArray(local_geometry_tags)) {
-          local_array.push(local_geometry_tags.join(", "));
-        } else {
-          local_array.push("");
-        }
-      }
-
-      //Actions bar
-      {
-        let actions_bar_el = local_geometry.getActionsBarElement();
-        let brush_button = veButton((v, e) => {
-          if (main.brush.selected_geometry?.id !== e.element.geometry?.id) {
-            main.brush.selected_geometry = e.element.geometry;
-            select_component.v = true;
-            _redrawSelections();
-          }
-        }, {
-          name: "<icon>brush</icon>",
-          tooltip: "Move to Brush"
-        });
-          brush_button.element.geometry = local_geometry;
-          actions_bar_el.prepend(brush_button.element);
-
-			  local_array.push(actions_bar_el);
-      }
-			
-			//Push local_array to table_array
-			table_array.push(local_array);
-      table_map[local_geometry.id] = {
-        geometry: local_geometry,
-        row: local_array
-      };
-		}
+				);
+				brush_button.element.geometry = local_geometry;
+				actions_bar_el.prepend(brush_button.element);
+				
+				local_array.push(actions_bar_el);
+				
+				return local_array;
+			},
+			table_options: {
+				disable_hide_columns: [0],
+			},
+		});
 		
-		//Return statement
-		return table_array;
+		this.interface = veInterface(
+			{
+				main_crud: this.crud,
+			},
+			{ is_folder: false }
+		);
+		
+		super.open("instance", {
+			can_rename: false,
+			name: this.layer.name ? this.layer.name : "Layer",
+		});
+		
+		// Return statement
+		return this.interface;
 	}
-}
+	
+	/**
+	 * Triggers a filter on the underlying CRUD component.
+	 */
+	filterGeometryTable(arg0_options) {
+		this.crud.filterTable(arg0_options);
+	}
+};
